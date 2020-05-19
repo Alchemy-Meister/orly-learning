bf1c5bf114e24fc6e37c6f362f3c79b4557d778f
Alchemy-Meister <jesusesma@gmail.com>
2020-05-19 19:01:57 +0200

    test session cookies after logged in

--- tests/auth_handler/test_login.py
+++ tests/auth_handler/test_login.py
@@ -12,25 +12,32 @@ def init_auth():
     return AuthHandler(None, None)
 
 @pytest.fixture(name='_response')
-def init_response():
+def init_response(logged_headers):
+    def login_callback(request):
+        request_body = json.loads(request.body)
+        if (
+                'valid' in request_body.get('email', '')
+                and 'valid' in request_body.get('password', '')
+        ):
+
+            return (HTTPStatus.OK.value, logged_headers, '')
+
+        return (HTTPStatus.BAD_REQUEST, {}, '')
+
     with responses.RequestsMock() as rsps:
         rsps.add_callback(
             responses.POST, AuthHandler.OREILLY_LOGIN, callback=login_callback
         )
         yield rsps
 
-def login_callback(request):
-    request_body = json.loads(request.body)
-    if (
-            'valid' in request_body.get('email', '')
-            and 'valid' in request_body.get('password', '')
-    ):
-        return (HTTPStatus.OK.value, {}, '')
-
-    return (HTTPStatus.BAD_REQUEST, {}, '')
-
-def test_authorized_login(auth, _response):
-    auth.login('valid-email', 'valid-password')
+def test_authorized_login(auth, _response, logged_headers):
+    session = auth.login('valid-email', 'valid-password')
+    session_cookie_names = list(session.cookies.get_dict().keys())
+    headers_cookies = logged_headers['set-cookie']
+    assert all(
+        cookie_name in headers_cookies
+        for cookie_name in session_cookie_names
+    )
 
 def test_unauthorized_login(auth, _response):
     with pytest.raises(InvalidCredentials):

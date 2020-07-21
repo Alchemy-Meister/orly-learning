392ccb21502835797904582ca09dd090fe74360f
Alchemy-Meister <jesusesma@gmail.com>
2020-07-21 23:07:53 +0200

    allow session optional as optional constructor argument

--- src/orlylearning/handler/abstract_handler.py
+++ src/orlylearning/handler/abstract_handler.py
@@ -1,9 +1,12 @@
 from abc import ABC
+from typing import Optional
+
+from requests import Session
 
 from ..exceptions import InvalidSession
 
 class AbstractHandler(ABC):
-    def __init__(self, session):
+    def __init__(self, session: Optional[Session] = None):
         self.session = session
 
     def _check_session(self):

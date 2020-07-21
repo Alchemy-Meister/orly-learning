d51850db4cc5da259772df02410b1b442f658c25
Alchemy-Meister <jesusesma@gmail.com>
2020-07-21 23:02:54 +0200

    implemented MissingRegistrationFields

--- src/orlylearning/exceptions.py
+++ src/orlylearning/exceptions.py
@@ -4,6 +4,13 @@ class EmailError(Exception):
 class PasswordError(Exception):
     pass
 
+class MissingRegistrationFields(Exception):
+    def __init__(self, missing_field, compulsory_fields):
+        super().__init__(
+            f'missing {repr(missing_field)} registration field, '
+            f'the following fields are required: {compulsory_fields}'
+        )
+
 class InvalidCredentials(Exception):
     def __init__(self):
         super().__init__('email or password are incorrect')

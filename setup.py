7adc20984d6f5b6b11ca0d1cf8a3e50da50cc5f4
Alchemy-Meister <jesusesma@gmail.com>
2020-09-16 17:38:06 +0200

    added license

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#! /usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2020 Alchemy-Meister
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from setuptools import setup, find_packages
 
 install_requires = [
